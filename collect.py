95c3363baae692edcee1fd5cf7aa8c3baa9ff0fc
username <exia@isimarkets.com>
2022-03-16 14:48:07 +0800

    implement collect.py

--- collect.py
+++ collect.py
@@ -189,6 +189,7 @@ def bpa_send_any_log(repo       : str            = "CECNdata/anylog" ,
             final_command          = []
             atp_name               = os.path.basename(real_path).strip()
             stime                  = datetime.datetime.now().strftime("%Y%m%d%H%M%ST%H")
+            print(current_script_name)
             if current_script_name == "pdp.py":
                 head_filename="PDP"
                 log_path_list      = [
