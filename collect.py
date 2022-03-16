9a0af93cb10b0765c9a0edfd2585bbd5642ada2d
username <exia@isimarkets.com>
2022-03-16 09:11:20 +0800

    add collect.py

---
+++ collect.py
@@ -0,0 +1,205 @@
+#!/usr/bin/env python
+# coding=utf-8
+import requests
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+from pprint import pprint, pformat
+import multiprocessing, threading
+import base64
+import datetime
+import hashlib
+import json
+import logging
+import traceback
+import os
+import random
+import re
+import shutil
+import sys
+import time
+import functools
+import csv
+import uuid
+import io
+
+
+"""
+    @brief: init common vars
+"""
+#↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓
+today = datetime.datetime.utcnow()
+cn_today = today + datetime.timedelta(hours=8)
+#↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑
+
+
+"""
+    @brief: logger init and give a global logger <Elogger>
+    @get:   Elogger
+"""
+#↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓
+class TerminalColour:
+    """
+    Terminal colour formatting codes
+    """
+    # https://stackoverflow.com/questions/287871/print-in-terminal-with-colors
+    MAGENTA   = '\033[95m'
+    BLUE      = '\033[94m'
+    GREEN     = '\033[92m'
+    YELLOW    = '\033[93m'
+    PINK      = '\033[45m'
+    RED       = '\033[91m'
+    GREY      = '\033[0m'  # normal
+    WHITE     = '\033[1m'  # bright white
+    UNDERLINE = '\033[4m'
+
+# logging.Formatter.converter = time.gmtime # force to use gmt time
+logging.basicConfig(
+    format  = f'<<%(asctime)s>>[%(levelname)8s] - %(message)s\t\t{TerminalColour.GREEN}PID-%(process)s:{TerminalColour.GREEN}%(name)s:%(funcName)s:%(pathname)s:%(lineno)d{TerminalColour.GREY}',
+    datefmt =  '%Y-%m-%d %H:%M:%S %Z',
+)
+
+logging.addLevelName(logging.INFO     , "{}{}{}".format(TerminalColour.GREEN   , logging.getLevelName(logging.INFO)     , TerminalColour.GREY))
+logging.addLevelName(logging.WARNING  , "{}{}{}".format(TerminalColour.YELLOW  , logging.getLevelName(logging.WARNING)  , TerminalColour.GREY))
+logging.addLevelName(logging.ERROR    , "{}{}{}".format(TerminalColour.RED     , logging.getLevelName(logging.ERROR)    , TerminalColour.GREY))
+logging.addLevelName(logging.CRITICAL , "{}{}{}".format(TerminalColour.MAGENTA , logging.getLevelName(logging.CRITICAL) , TerminalColour.GREY))
+logging.addLevelName(logging.DEBUG    , "{}{}{}".format(TerminalColour.PINK    , logging.getLevelName(logging.DEBUG)    , TerminalColour.GREY))
+
+log_level = "DEBUG" if "--debug" in sys.argv or os.getenv("LOG_LEVEL") == "debug" else "INFO"
+Elogger   = logging.getLogger("<Eloco>")
+Elogger.setLevel(log_level)
+#↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑
+
+
+"""
+    @brief: get the md5 of a file or folder
+"""
+def get_md5(file_path: str,
+            logger: logging.Logger = Elogger
+            ):
+    try:
+        if os.path.isfile(file_path):
+            md5 = hashlib.md5()
+            with open(file_path, 'rb') as f:
+                for chunk in iter(lambda: f.read(4096), b""):
+                    md5.update(chunk)
+            return(md5.hexdigest()) # return files md5
+        elif os.path.isdir(file_path):
+            MD5File = "{}_tmp.md5".format(str(random.randint(0, 1000)).zfill(6))
+            with open(MD5File, 'w') as outfile:
+                md5_lst=[]
+                for root, _, files in os.walk(file_path):
+                    for file in files:
+                        filefullpath = os.path.join(root, file)
+                        md5 = get_md5(file_path=filefullpath)
+                        md5_lst.append([md5,file])
+                md5_lst=sorted(md5_lst)
+                for md5 in md5_lst:
+                    outfile.write(md5[0]+md5[1])
+            val = get_md5(MD5File)
+            os.remove(MD5File)
+            return(val) # return folders md5
+    except Exception as e:
+        logger.error(e)
+        logger.debug(traceback.format_exc())
+        return(False)
+
+
+"""
+    @brief:  calc md5 from ./downloads, and remove the same md5 and record the new to index.txt
+    @return: success or failure
+"""
+#↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓
+def bpa_index_md5_check(logger: logging.Logger = Elogger
+                       ) -> bool:
+    try:
+        os.chdir(sys.path[0])
+        index_path = "./index.txt"
+        md5_json = json.loads(open(index_path).read()
+                              ) if os.path.exists(index_path) else {}
+
+        def walk_file_index(md5_json: dict = md5_json) -> dict:
+            L = []
+            summary = {}
+            summary["same"] = {}
+            summary["downloaded"] = {}
+            for root, _, files in os.walk("downloads"):
+                for file in files:
+                    file_dir = os.path.join(root, file)
+                    md5 = get_md5(file_path=file_dir)
+                    if file in md5_json:
+                        if md5 == md5_json[file]:
+                            summary["same"][file] = 0
+                            os.remove(file_dir)
+                            logger.info(f"[MD5]:\t<{file_dir}> has same md5, now del")
+                        else:
+                            summary["downloaded"][file] = 0
+                            md5_json[file] = md5
+                    else:
+                        md5_json[file] = md5
+                        summary["downloaded"][file] = 0
+            with open("summary.txt", "w") as f:
+                f.write(json.dumps(summary))
+            return(md5_json)
+
+        # main function here
+        if os.path.exists(index_path):
+            md5_json = walk_file_index()
+            with open(index_path, "w") as f:
+                f.write(json.dumps(md5_json))
+            return(True)
+        else:
+            logger.warning(f"<{index_path}> not exists,{{index_md5_check}} failure")
+            return(False)
+    except Exception as e:
+        logger.error(e)
+        logger.debug(traceback.format_exc())
+        return(False)
+#↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑
+
+
+"""
+    @brief:  init request proxy from options.json
+    @return: success or failure
+"""
+def bpa_init_request_proxy(test_proxy_url: str = "https://www.bing.com/",
+                           test_proxy_force_200: bool = False,
+                           test_timeout: int = 30,
+                           logger: logging.Logger = Elogger
+                          ) -> requests.sessions.Session: 
+    try:
+        s = requests.Session()
+        s.verify = False  # disable ssl verify
+        os.chdir(sys.path[0])
+        atp_name = os.path.basename(os.path.abspath(os.path.join(os.getcwd(), "../..")))
+        # reading the proxy settings from {atp}.json
+        atp_json_path = f"../../{atp_name}.json"
+        if os.path.exists(atp_json_path):
+            atp_config = json.loads(open(atp_json_path, "r").read())
+            proxy_config = atp_config["proxy"].strip()
+            if "NO" != proxy_config and "" != proxy_config:
+                proxy_config = atp_config["proxy"].split("--proxy-auth=")
+                if len(proxy_config) == 2:  # add auth to proxy_string
+                    auth_string = proxy_config[1].strip()
+                    proxy_config[0] = auth_string+"@"+proxy_config[0]
+                proxy_string = "http://" + \
+                    proxy_config[0] if "http://" not in proxy_config[0] else proxy_config[0]
+                proxy_string = proxy_string.strip()
+                logger.info(f"[proxy] use proxy: `{proxy_string}`")
+                s.proxies = {
+                    "http": proxy_string,
+                    "https": proxy_string
+                }
+                test_proxy = s.get(test_proxy_url, timeout=test_timeout)
+                logger.info(f"[proxy] `{test_proxy_url}` proxy code: {str(test_proxy.status_code)}")
+                if test_proxy_force_200:
+                    test_proxy.raise_for_status()
+    except Exception as e:
+        logger.error(e)
+        logger.debug(traceback.format_exc())
+        s.proxies = None
+        s.auth = None
+    s.verify = False  # disable ssl verify
+    return(s)
+
+
+
