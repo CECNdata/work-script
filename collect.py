5cb2336920a3bb4195fbc201bad1e195da67e92f
username <exia@isimarkets.com>
2022-03-16 15:38:38 +0800

    implement collect.py

--- collect.py
+++ collect.py
@@ -223,13 +223,10 @@ def bpa_send_any_log(repo       : str            = "CECNdata/anylog" ,
             pid=os.fork()
             if pid==0: # new process
                 for command in final_commands:
-                    print(command)
                     final_command=command.replace('{slash}',chr(92))
-                    print(final_command)
                     final_command = f"""bash -c 'nohup sleep {time_sleep}s;{command} &' & > /dev/null """ 
                     logger.debug(f"uploading log with <{final_command}>")
-                    print([final_command])
-                    #os.system(final_command)
+                    os.system(final_command)
             return(True)
     except Exception as e:
         logger.error(e)
